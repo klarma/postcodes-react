b3f256ae52bd61b18ca10a91c46047085cc9ad43
Katarzyna Napierala <k_napierala@op.pl>
2022-04-03 12:53:02 +0200

    logic of calling fetchRandomPostcode corrected

--- src/RandomPostCode/index.js
+++ src/RandomPostCode/index.js
@@ -1,44 +1,35 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 const RandomPostcode = () => {
-  const [activeComponent, setActiveComponent] = useState(false);
   const [postcodes, setPostcodes] = useState({
     state: "loading",
     randomPostcode: null,
   });
 
-  useEffect(() => {
-    const fetchRandomPostcode = async () => {
-      try {
-        const response = await fetch(
-          "https://api.postcodes.io/random/postcodes"
-        );
+  const fetchRandomPostcode = async () => {
+    try {
+      const response = await fetch("https://api.postcodes.io/random/postcodes");
 
-        if (!response.ok) {
-          throw new Error(response.statusText);
-        }
-
-        const { result } = await response.json();
-
-        setPostcodes({
-          status: "success",
-          randomPostcode: result.postcode,
-        });
-      } catch (error) {
-        setPostcodes({ status: "error" });
-        console.error(error);
+      if (!response.ok) {
+        throw new Error(response.statusText);
       }
-    };
 
-    fetchRandomPostcode();
-  }, [activeComponent]);
+      const { result } = await response.json();
+
+      setPostcodes({
+        status: "success",
+        randomPostcode: result.postcode,
+      });
+    } catch (error) {
+      setPostcodes({ status: "error" });
+      console.error(error);
+    }
+  };
 
   return (
     <>
-      <button onClick={() => setActiveComponent(!activeComponent)}>
-        Draw UK postcode
-      </button>
-      <p>{postcodes.randomPostcode}</p>
+      <button onClick={fetchRandomPostcode}>Draw UK postcode</button>
+      <p>{postcodes.randomPostcode ? postcodes.randomPostcode : "PL1 3BW"}</p>
     </>
   );
 };
